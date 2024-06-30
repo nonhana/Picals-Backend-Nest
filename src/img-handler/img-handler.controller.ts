800329d293075212aa3207d7d900e7488f3377ea
non_hana <1209220829@qq.com>
2024-06-30 15:07:43 +0800

    feat: 移除node-canvas服务端绘图

--- src/img-handler/img-handler.controller.ts
+++ src/img-handler/img-handler.controller.ts
@@ -14,8 +14,8 @@ export class ImgHandlerController {
 		return await this.imgHandlerService.generateThumbnail(imageBuffer, fileName);
 	}
 
-	@Get('get-default-image')
-	async getDefaultImage(@Query('text') text: string, @Query('color') color?: string) {
-		return await this.imgHandlerService.getDefaultImage(text, color);
-	}
+	// @Get('get-default-image')
+	// async getDefaultImage(@Query('text') text: string, @Query('color') color?: string) {
+	// 	return await this.imgHandlerService.getDefaultImage(text, color);
+	// }
 }
--- src/img-handler/img-handler.service.ts
+++ src/img-handler/img-handler.service.ts
@@ -1,7 +1,7 @@
 import { Inject, Injectable } from '@nestjs/common';
 import * as sharp from 'sharp';
-import { TinyColor } from '@ctrl/tinycolor';
-import { createCanvas, registerFont, type CanvasRenderingContext2D } from 'canvas';
+// import { TinyColor } from '@ctrl/tinycolor';
+// import { createCanvas, registerFont, type CanvasRenderingContext2D } from 'canvas';
 import * as fs from 'fs';
 import * as path from 'path';
 import { R2Service } from 'src/r2/r2.service';
@@ -13,36 +13,36 @@ export class ImgHandlerService {
 
 	constructor() {}
 
-	isWarmHue = (color: string): boolean => {
-		const colorObj = new TinyColor(color);
-		return colorObj.isLight();
-	};
+	// isWarmHue = (color: string): boolean => {
+	// 	const colorObj = new TinyColor(color);
+	// 	return colorObj.isLight();
+	// };
 
-	randomColor = () => {
-		return `#${Math.floor(Math.random() * 16777215)
-			.toString(16)
-			.padStart(6, '0')}`;
-	};
+	// randomColor = () => {
+	// 	return `#${Math.floor(Math.random() * 16777215)
+	// 		.toString(16)
+	// 		.padStart(6, '0')}`;
+	// };
 
-	wrapText = (context: CanvasRenderingContext2D, text: string, maxWidth: number): string[] => {
-		const words = text.split(' ');
-		let line = '';
-		const lines = [];
+	// wrapText = (context: CanvasRenderingContext2D, text: string, maxWidth: number): string[] => {
+	// 	const words = text.split(' ');
+	// 	let line = '';
+	// 	const lines = [];
 
-		for (let n = 0; n < words.length; n++) {
-			const testLine = line + words[n] + ' ';
-			const metrics = context.measureText(testLine);
-			const testWidth = metrics.width;
-			if (testWidth > maxWidth && n > 0) {
-				lines.push(line);
-				line = words[n] + ' ';
-			} else {
-				line = testLine;
-			}
-		}
-		lines.push(line);
-		return lines;
-	};
+	// 	for (let n = 0; n < words.length; n++) {
+	// 		const testLine = line + words[n] + ' ';
+	// 		const metrics = context.measureText(testLine);
+	// 		const testWidth = metrics.width;
+	// 		if (testWidth > maxWidth && n > 0) {
+	// 			lines.push(line);
+	// 			line = words[n] + ' ';
+	// 		} else {
+	// 			line = testLine;
+	// 		}
+	// 	}
+	// 	lines.push(line);
+	// 	return lines;
+	// };
 
 	// 压缩图片为缩略图
 	async generateThumbnail(imageBuffer: Buffer, fileName: string) {
@@ -70,41 +70,41 @@ export class ImgHandlerService {
 	}
 
 	// 生成默认图片
-	async getDefaultImage(text: string, color?: string): Promise<string> {
-		const fontPath = path.resolve(
-			__dirname,
-			'..',
-			'..',
-			'assets',
-			'fonts',
-			'NotoSansSC-Regular.ttf',
-		);
-		registerFont(fontPath, { family: 'Noto Sans SC', weight: 'regular' });
+	// async getDefaultImage(text: string, color?: string): Promise<string> {
+	// 	const fontPath = path.resolve(
+	// 		__dirname,
+	// 		'..',
+	// 		'..',
+	// 		'assets',
+	// 		'fonts',
+	// 		'NotoSansSC-Regular.ttf',
+	// 	);
+	// 	registerFont(fontPath, { family: 'Noto Sans SC', weight: 'regular' });
 
-		const imageSize = 600;
-		const bgColor = color || this.randomColor();
-		const textColor = this.isWarmHue(bgColor) ? '#3d3d3d' : '#fff';
+	// 	const imageSize = 600;
+	// 	const bgColor = color || this.randomColor();
+	// 	const textColor = this.isWarmHue(bgColor) ? '#3d3d3d' : '#fff';
 
-		const canvas = createCanvas(imageSize, imageSize);
-		const context = canvas.getContext('2d');
-		context.fillStyle = bgColor;
-		context.fillRect(0, 0, imageSize, imageSize);
-		context.font = '64px "Noto Sans SC"';
-		context.fillStyle = textColor;
-		context.textAlign = 'center';
-		context.textBaseline = 'middle';
-		context.fillText(text, imageSize / 2, imageSize / 2);
+	// 	const canvas = createCanvas(imageSize, imageSize);
+	// 	const context = canvas.getContext('2d');
+	// 	context.fillStyle = bgColor;
+	// 	context.fillRect(0, 0, imageSize, imageSize);
+	// 	context.font = '64px "Noto Sans SC"';
+	// 	context.fillStyle = textColor;
+	// 	context.textAlign = 'center';
+	// 	context.textBaseline = 'middle';
+	// 	context.fillText(text, imageSize / 2, imageSize / 2);
 
-		const buffer = canvas.toBuffer('image/jpeg');
+	// 	const buffer = canvas.toBuffer('image/jpeg');
 
-		// 将图片保存到R2并返回URL
-		const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9) + '-' + text;
-		const outputPath = path.join(__dirname, 'uploads', uniqueSuffix + '-default.jpg');
-		if (!fs.existsSync(path.dirname(outputPath))) {
-			fs.mkdirSync(path.dirname(outputPath), { recursive: true });
-		}
-		await sharp(buffer).toFile(outputPath);
-		const targetPath = 'images' + outputPath.split('uploads')[1].replace(/\\/g, '/');
-		return await this.r2Service.uploadFileToR2(outputPath, targetPath);
-	}
+	// 	// 将图片保存到R2并返回URL
+	// 	const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9) + '-' + text;
+	// 	const outputPath = path.join(__dirname, 'uploads', uniqueSuffix + '-default.jpg');
+	// 	if (!fs.existsSync(path.dirname(outputPath))) {
+	// 		fs.mkdirSync(path.dirname(outputPath), { recursive: true });
+	// 	}
+	// 	await sharp(buffer).toFile(outputPath);
+	// 	const targetPath = 'images' + outputPath.split('uploads')[1].replace(/\\/g, '/');
+	// 	return await this.r2Service.uploadFileToR2(outputPath, targetPath);
+	// }
 }
