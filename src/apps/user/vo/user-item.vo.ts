013386fcacd8c6b310e1dce381b4e0416ac9bf63
non_hana <1209220829@qq.com>
2024-07-04 23:50:44 +0800

    feat: UserItem 新增 email 字段

--- src/apps/user/vo/user-item.vo.ts
+++ src/apps/user/vo/user-item.vo.ts
@@ -3,6 +3,7 @@ import type { User } from '../entities/user.entity';
 export class UserItemVo {
 	id: string;
 	username: string;
+	email: string;
 	avatar: string;
 	intro: string;
 	isFollowing: boolean;
@@ -11,6 +12,7 @@ export class UserItemVo {
 	constructor(user: User, isFollowing: boolean) {
 		this.id = user.id;
 		this.username = user.username;
+		this.email = user.email;
 		this.avatar = user.avatar;
 		this.intro = user.signature;
 		this.isFollowing = isFollowing;
