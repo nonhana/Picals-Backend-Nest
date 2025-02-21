0140d93809e7904b45bbbfa96e56ae6caa0f88ee
non_hana <1209220829@qq.com>
2025-02-21 09:55:58 +0800

    feat: 配置 CORS 选项，支持自定义源和请求方法

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,12 @@ async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
 	app.setGlobalPrefix('api');
 	const configConfig = app.get(ConfigService);
-	app.enableCors();
+	app.enableCors({
+		origin: configConfig.get('CORS_ORIGIN'),
+		methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+		allowedHeaders: ['Content-Type', 'Authorization'],
+		credentials: true,
+	});
 
 	await app.listen(configConfig.get('NEST_PORT'));
 }
